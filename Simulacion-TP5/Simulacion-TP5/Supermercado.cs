fe518913691a27ff433de2b8fe43e0729cbcd3d3
mariafdiaz <flordiaz.9.10@gmail.com>
2017-06-13 16:52:47 -0300

    Add files via upload
    Acomode supermercado, agregue metodos para el recorrido

--- Simulacion-TP5/Simulacion-TP5/Supermercado.cs
+++ Simulacion-TP5/Simulacion-TP5/Supermercado.cs
@@ -9,61 +9,30 @@ namespace Simulacion_TP5
 {
     class Supermercado
     {
-        private Random RND = new Random();
-        //EL MOMENTO DE TIEMPO EN EL QUE OCURRE CADA EVENTO//
+        //VARIABLES//
+        private Random RND = new Random();  //EL MOMENTO DE TIEMPO EN EL QUE OCURRE CADA EVENTO//
         private double reloj; //declaramos el reloj 
-
-        public double Reloj  //constructor    
-        {
-            get { return reloj; }
-            set { reloj = value; }
-        }
-       
-        
-
-        //NUMERO ALEATORIO PARA SABER LA PROXIMA LLEGADA//
         private double aleatorioLlegadaCliente; //es el rnd que se muestra que calcula el tiempo entre llegadas
-
-        public double AleatorioLlegadaCliente //constructor
-        {
-            get { return aleatorioLlegadaCliente; }
-            set { aleatorioLlegadaCliente = value; }
-
-        }
-        //TIEMPO HASTA QUE LLEGUE UN CLIENTE
-        private double llegadaCliente;
-
-        public double LlegadaCliente  //constructor    
-        {
-            get { return llegadaCliente; }
-            set { llegadaCliente = value; }
-        }
-        //MOMENTO DE LA PROXIMA LLEGADA DE UN CLIENTE//
-        private double proximaLlegadaCliente; //el tiempo en el que llegara el proximo pasajero,que se calcula con el metodo calcular tiempo proxima llegada, pasandole como tiempo actual el reloj de el evento ocurrido y sumandole la cantidad de minutos para proxima llegada de pasajero
-
-        public double ProximaLlegadaCliente  //constructor    
-        {
-            get { return proximaLlegadaCliente; }
-            set { proximaLlegadaCliente = value; }
-        }
+        private double llegadaCliente; //tiempo en que llega un cliente
+        private double proximaLlegadaCliente; //proxima llegada del cliente - Metodo calcular tiempo proxima llegada(parametro reloj actual + cant de minutos para prox llegada de pasajero)
+        private double aleatorioRecorrido; 
+        private double idRecorrido;
 
 
-        private double aleatorioRecorrido; //es el rnd que se muestra que calcula el tiempo entre llegadas
 
-        public double AleatorioRecorrido //constructor
-        {
-            get { return aleatorioRecorrido; }
-            set { aleatorioRecorrido = value; }
 
-        }
-        private double idRecorrido; //el tiempo en el que llegara el proximo pasajero,que se calcula con el metodo calcular tiempo proxima llegada, pasandole como tiempo actual el reloj de el evento ocurrido y sumandole la cantidad de minutos para proxima llegada de pasajero
+        //GET Y SET//
+        public double Reloj { get {return reloj; } set { reloj = value; }} 
+        public double AleatorioLlegadaCliente { get { return aleatorioLlegadaCliente; } set { aleatorioLlegadaCliente = value; }}     
+        public double LlegadaCliente { get { return llegadaCliente; } set { llegadaCliente = value; }}   
+        public double ProximaLlegadaCliente { get { return proximaLlegadaCliente; } set { proximaLlegadaCliente = value; }}
+        public double AleatorioRecorrido { get { return aleatorioRecorrido; }    set { aleatorioRecorrido = value; }}    
+        public double IDRecorrido { get { return idRecorrido; } set { idRecorrido = value; }}
 
-        public double IDRecorrido  //constructor    
-        {
-            get { return idRecorrido; }
-            set { idRecorrido = value; }
-        }
 
+
+
+        //METODOS MATEMATICOS//
         public double generarPoisson(double lambda, double RND)
         {
             double p = 1;
@@ -92,5 +61,28 @@ namespace Simulacion_TP5
 
         }
 
-    }
+
+
+        //METODOS PARA LOS RECORRIDOS//
+        private int generarRecorrido(double rnd) //para ver en donde cae el aleatorio
+        {
+            int idRecorrido=0;
+            if (rnd >= 0 || rnd < 20) { idRecorrido = 1; } //Verduleria-Panaderia
+            if (rnd >= 30 || rnd < 50) { idRecorrido = 2; } //Verduleria-Carniceria-Gondola 
+            if (rnd >= 50 || rnd < 60) { idRecorrido = 3; } //Panaderia
+            if (rnd >= 60 || rnd < 80) { idRecorrido = 4; }//Carniceria-Panaderia-Gondola-Verduleria
+            if (rnd >= 80 || rnd < 100) { idRecorrido = 5; }//Gondola
+            return idRecorrido;
+        }
+
+        public double generarAleatorio() 
+        {
+            Random RND = new Random();
+            double rnd = Math.Round(RND.NextDouble(), 4);
+            return rnd;
+
+        }
+
+
+}
 }
